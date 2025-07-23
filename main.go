25c071f4136b16f006093db5520571f95d1e76a2
gram <git@orsinium.dev>
2025-07-23 21:18:57 +0200

    set the stage

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "github.com/firefly-zero/firefly-go/firefly"
 
+var (
+	imgBg      firefly.Image
+	imgCat     firefly.Image
+	imgDone    firefly.Image
+	imgHolder  firefly.Image
+	imgHolder2 firefly.Image
+	imgPaws    firefly.Image
+	imgRoll    firefly.Image
+	imgStripe  firefly.Image
+)
+
 func init() {
 	firefly.Boot = boot
 	firefly.Update = update
@@ -9,7 +20,7 @@ func init() {
 }
 
 func boot() {
-	// ...
+	loadAssets()
 }
 
 func update() {
@@ -17,15 +28,30 @@ func update() {
 }
 
 func render() {
-	firefly.ClearScreen(firefly.ColorWhite)
-	firefly.DrawTriangle(
-		firefly.Point{X: 60, Y: 10},
-		firefly.Point{X: 40, Y: 40},
-		firefly.Point{X: 80, Y: 40},
-		firefly.Style{
-			FillColor:   firefly.ColorDarkBlue,
-			StrokeColor: firefly.ColorBlue,
-			StrokeWidth: 1,
-		},
-	)
+	firefly.DrawImage(imgBg, firefly.Point{})
+	firefly.DrawImage(imgCat, firefly.Point{X: 166, Y: 39})
+	firefly.DrawImage(imgHolder, firefly.Point{X: 63, Y: 68})
+
+	stripe := imgStripe.Sub(firefly.Point{}, firefly.Size{W: 53, H: 240})
+	firefly.DrawSubImage(stripe, firefly.Point{X: 93, Y: 71})
+
+	roll := imgRoll.Sub(firefly.Point{}, firefly.Size{W: 84, H: 76})
+	firefly.DrawSubImage(roll, firefly.Point{X: 70, Y: 31})
+
+	firefly.DrawImage(imgHolder2, firefly.Point{X: 63, Y: 68})
+
+	paws := imgPaws.Sub(firefly.Point{}, firefly.Size{W: 69, H: 82})
+	firefly.DrawSubImage(paws, firefly.Point{X: 125, Y: 51})
+
+}
+
+func loadAssets() {
+	imgBg = firefly.LoadFile("bg", nil).Must().Image()
+	imgCat = firefly.LoadFile("cat", nil).Must().Image()
+	imgDone = firefly.LoadFile("done", nil).Must().Image()
+	imgHolder = firefly.LoadFile("holder", nil).Must().Image()
+	imgHolder2 = firefly.LoadFile("holder2", nil).Must().Image()
+	imgPaws = firefly.LoadFile("paws", nil).Must().Image()
+	imgRoll = firefly.LoadFile("roll", nil).Must().Image()
+	imgStripe = firefly.LoadFile("stripe", nil).Must().Image()
 }
